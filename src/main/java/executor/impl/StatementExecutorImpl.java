6c74b0ac9c2e169fce8a9e980db354a455f697e0
theghostwhoforks <flawed.flint@gmail.com>
2014-03-21 13:12:49 +0530

    logging at key code points

--- src/main/java/executor/impl/StatementExecutorImpl.java
+++ src/main/java/executor/impl/StatementExecutorImpl.java
@@ -4,19 +4,25 @@ import constant.Messages;
 import exception.MetaDataServiceRuntimeException;
 import executor.StatementExecutor;
 import model.Query;
+import org.apache.log4j.Logger;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class StatementExecutorImpl implements StatementExecutor {
+    private static Logger logger = Logger.getLogger(StatementExecutor.class);
 
     @Override
     public boolean createTable(Query query, Connection connection) {
+        String sql = query.asSql();
+        logger.info(String.format("Creating table. Query - %s",sql));
+
         try {
-            PreparedStatement statement = connection.prepareStatement(query.asSql());
+            PreparedStatement statement = connection.prepareStatement(sql);
             return statement.execute();
         } catch (SQLException e) {
+            logger.error(String.format("There was an error while creating a table. Offending query  -%s",sql));
             throw new MetaDataServiceRuntimeException(Messages.CREATE_TABLE_ERROR,e);
         }
     }
--- src/main/java/service/impl/SqlServiceImpl.java
+++ src/main/java/service/impl/SqlServiceImpl.java
@@ -3,12 +3,14 @@ package service.impl;
 import builder.QueryBuilder;
 import executor.StatementExecutor;
 import model.Query;
+import org.apache.log4j.Logger;
 import service.SqlService;
 
 import java.sql.Connection;
 
 public class SqlServiceImpl implements SqlService {
 
+    private static Logger logger = Logger.getLogger(SqlService.class);
     private final StatementExecutor executor;
 
     public SqlServiceImpl(StatementExecutor executor){
@@ -17,6 +19,7 @@ public class SqlServiceImpl implements SqlService {
 
     @Override
     public boolean createTable(Connection connection, String data) {
+        logger.info(String.format("Creating Table. Data supplied - %s",data));
         Query query = QueryBuilder.with().formDefinition(data).build();
         return executor.createTable(query,connection);
     }
