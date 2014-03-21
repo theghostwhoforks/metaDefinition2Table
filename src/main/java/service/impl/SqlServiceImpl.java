0898affa512bc0dbea16581568257bbcae592351
theghostwhoforks <flawed.flint@gmail.com>
2014-03-21 18:11:10 +0530

    using slf4j logger

--- src/main/java/service/impl/SqlServiceImpl.java
+++ src/main/java/service/impl/SqlServiceImpl.java
@@ -3,14 +3,12 @@ package service.impl;
 import builder.QueryBuilder;
 import executor.StatementExecutor;
 import model.Query;
-import org.apache.log4j.Logger;
 import service.SqlService;
 
 import java.sql.Connection;
 
 public class SqlServiceImpl implements SqlService {
-
-    private static Logger logger = Logger.getLogger(SqlService.class);
+    private static org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(SqlService.class);
     private final StatementExecutor executor;
 
     public SqlServiceImpl(StatementExecutor executor){
@@ -19,7 +17,7 @@ public class SqlServiceImpl implements SqlService {
 
     @Override
     public boolean createTable(Connection connection, String data) {
-        logger.info(String.format("Creating Table. Data supplied - %s",data));
+        logger.info("Creating Table. Data supplied - {}",data);
         Query query = QueryBuilder.with().formDefinition(data).build();
         return executor.createTable(query,connection);
     }
