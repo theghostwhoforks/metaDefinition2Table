65cac3ceab431053756c99bc4a4ce7f177b15057
kashishm <kashishmunjal64@gmail.com>
2014-04-07 20:08:31 +0530

    adding test cases for ResultSetWrapper

---
+++ src/test/java/wrapper/ResultSetWrapperTest.java
@@ -0,0 +1,45 @@
+package wrapper;
+
+import exception.MetaDataServiceRuntimeException;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ResultSetWrapperTest {
+
+    @Rule
+    public ExpectedException thrown= ExpectedException.none();
+
+    @Test
+    public void throwsExceptionWhenItDoesNotFindColumn() throws Exception {
+        ResultSet resultSet = mock(ResultSet.class);
+        ResultSetMetaData resultSetMetaData = mock(ResultSetMetaData.class);
+
+        thrown.expect(MetaDataServiceRuntimeException.class);
+
+        when(resultSet.next()).thenThrow(new SQLException());
+        when(resultSet.getMetaData()).thenReturn(resultSetMetaData);
+
+        new ResultSetWrapper(resultSet).getParentTableFields();
+    }
+
+    @Test
+    public void throwsExceptionWhenItDoesNotFindColumnForSubForms() throws Exception {
+        ResultSet resultSet = mock(ResultSet.class);
+        ResultSetMetaData resultSetMetaData = mock(ResultSetMetaData.class);
+
+        thrown.expect(MetaDataServiceRuntimeException.class);
+
+        when(resultSet.next()).thenThrow(new SQLException());
+        when(resultSet.getMetaData()).thenReturn(resultSetMetaData);
+
+        new ResultSetWrapper(resultSet).getSubForm(null);
+    }
+}
