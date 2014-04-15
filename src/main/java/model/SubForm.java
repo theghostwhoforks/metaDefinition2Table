e3e72df5a37d480c4f6a6f8813fe7cfcba6e8449
theghostwhoforks <flawed.flint@gmail.com>
2014-04-15 15:19:26 +0530

    adding sample form with empty sub form instances

--- src/main/java/model/SubForm.java
+++ src/main/java/model/SubForm.java
@@ -5,6 +5,7 @@ import com.google.gson.annotations.SerializedName;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class SubForm {
@@ -15,7 +16,7 @@ public class SubForm {
     private List<Field> fields = new ArrayList<>();
 
     @SerializedName(value = "instances")
-    private List<Map<String, String>> instances;
+    private List<Map<String, String>> instances = new ArrayList<>();
 
     public SubForm() {
     }
